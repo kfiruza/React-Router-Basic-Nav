4491f1d07060988dccfcb6c5abf2afeba0be3fd1
khashimovafiruza@gmail.com <khashimovafiruza@gmail.com>
2020-01-27 21:50:50 -0600

    MVP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route } from 'react-router-dom'
 
-import { Home, About, Contact, Navigation } from './components';
 import './App.css';
+import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
-<div>
+  <div>
     <Navigation />
-    Output:
-    <Route exact path= "/">
+
+    <Route exact path="/">
       <Home />
     </Route>
-    <Route path= "/about">
+    <Route path="/about">
       <About />
     </Route>
-    <Route path= "/contact">
+    <Route path="/contact">
       <Contact />
     </Route>
   </div>
-
 );
 
 export default App;
